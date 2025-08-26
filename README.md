b9777c3c00af42c6dc175e64eae24432dc31ca84
foxbits <marian@foxbites.net>
2025-08-26 23:51:15 +0300

    Add default rawr page

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is a simple boilerplate application written with React (Next.Js) that aims 
 
 Pre-requisites: docker, docker compose, Node.js.
 
+Add your own pages using the [app router](https://nextjs.org/docs/app) rules (if you want a certain page to load first by default, use url rewrite rules through nginx or cloudflare). 
+
 Use standard commands - `npm run dev`, `npm run build` etc.
 
 ### Deployment
---
+++ src/app/noapteadesanziene/page.tsx
@@ -0,0 +1,14 @@
+import { UniverseBackground } from "@/components/universe-background/UniverseBackground";
+
+export default function NoapteaDeSanzieneHome() {
+    return (
+        <UniverseBackground>
+            <div className="flex flex-col items-center">
+                <h1>rawr, work in progress...</h1>
+                <div className="flex flex-row gap-1">bite me at
+                    <a href="https://www.instagram.com/hizzashi/" className="hover:underline">@hizzashi</a>
+                </div>
+            </div>
+        </UniverseBackground>
+    );
+};
